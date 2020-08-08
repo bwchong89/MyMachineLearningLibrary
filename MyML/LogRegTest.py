7ff978063dd9407d87d5bd73d24192e2aeb1aa3c
Brad <bwchong89@gmail.com>
2020-08-07 20:25:29 -0400

    Added LogisticRegression.py

---
+++ MyML/LogRegTest.py
@@ -0,0 +1,44 @@
+from LogisticRegression import LogisticRegression
+import numpy as np
+
+'''
+#Testing Sigmoid function
+x = 100
+print(LogisticRegression.sigmoid(x))
+x = -100
+print(LogisticRegression.sigmoid(x))
+x = np.array([i for i in range(10)])
+print(list(map(LogisticRegression.sigmoid, x)))
+x = np.array([i for i in range(-10,10)]).reshape(20,1)
+print(x)
+print(np.where(LogisticRegression.sigmoid(x) >= .5, 1, 0))
+'''
+'''
+x = np.array([50,-50])
+z = np.array((2*x + 5))
+
+y = np.where(LogisticRegression.sigmoid(z) >= 0.5, 1, 0).reshape(len(x), 1)
+
+myLogReg = LogisticRegression()
+myLogReg.logistic_reg_fit(x, y)
+print(y)
+print("my coeff")
+myLogReg.get_coeff()
+myLogReg.predict(np.array([50,-50]))
+'''
+
+# Testing single variable logistic regression
+#x = np.array([i for i in range(1, 16)])
+#y = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+
+
+
+x = np.array([[1, 3], [2, 3], [3, 3], [3, 2], [4, 4], [1, 1], [1, 2], [2, 1]])
+y = np.array([0, 0, 0, 0, 0, 1, 1, 1])
+
+myLogReg = LogisticRegression()
+myLogReg.logistic_reg_fit(x, y)
+print("my coeff")
+myLogReg.get_coeff()
+#myLogReg.predict(x)
+myLogReg.predict(np.array([[2, 3], [1, 0.5], [1, 5]]))
---
+++ MyML/LogisticRegression.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class LogisticRegression:
+
+    def __init__(self):
+        self.w = []
+        self.b = 0
+        self.cost = []
+
+    def sigmoid(x):
+        return 1/(1+np.exp(-x))
+
+    def logistic_reg_fit(self, x, y, alpha=0.0001, iterations=100, regularization="none", lambd=0):
+        n = x.ndim
+        m = len(x)
+        self.w = np.zeros((n, 1))
+        self.b = 0
+        y = y.reshape(m, 1)
+        for i in range(iterations):
+            z = np.dot(x.reshape(m, n), self.w.reshape(n, 1)) + self.b
+            y_hat = LogisticRegression.sigmoid(z).reshape(m, 1)
+            y_hat = np.where(y_hat > 0.5, 1, 0)
+            dw = (1/m) * np.dot(x.reshape(m, n).T, (y_hat-y).reshape(m, 1))
+            db = (1/m) * np.sum((y_hat-y))
+            self.w -= alpha*dw
+            self.b -= alpha*db
+
+    def get_coeff(self):
+        print("b = {}, w = {}".format(self.b, self.w))
+
+    def predict(self, x):
+        n = x.ndim
+        m = len(x)
+        z = np.dot(x.reshape(m, n), self.w.reshape(n, 1)) + self.b
+        y_hat = LogisticRegression.sigmoid(z)
+        print(np.where(y_hat > 0.5, 1, 0))
+
