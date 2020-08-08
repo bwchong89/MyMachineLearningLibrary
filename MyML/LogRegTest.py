9e35e5a2c5cc3848540bfc8305c09c37c9b6ca64
Brad <bwchong89@gmail.com>
2020-08-08 10:34:42 -0400

    Added Cost and Regularization

--- MyML/LogRegTest.py
+++ MyML/LogRegTest.py
@@ -27,11 +27,17 @@ myLogReg.predict(np.array([50, -50]))
 
 
 # Testing single variable logistic regression
-#x = np.array([i for i in range(1, 16)])
-#y = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+x = np.array([i for i in range(1, 16)])
+y = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+
+myLogReg = LogisticRegression()
+myLogReg.logistic_reg_fit(x, y)
+print("my coeff")
+myLogReg.get_coeff()
+myLogReg.predict(x)
 
 
-
+#Testing multivariable logistic regression
 x = np.array([[1, 3], [2, 3], [3, 3], [3, 2], [4, 4], [1, 1], [1, 2], [2, 1]])
 y = np.array([0, 0, 0, 0, 0, 1, 1, 1])
 
@@ -39,5 +45,5 @@ myLogReg = LogisticRegression()
 myLogReg.logistic_reg_fit(x, y)
 print("my coeff")
 myLogReg.get_coeff()
-#myLogReg.predict(x)
+myLogReg.predict(x)
 myLogReg.predict(np.array([[2, 3], [1, 0.5], [1, 5]]))
